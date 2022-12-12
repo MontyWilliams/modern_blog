2921eebddf0fbbf8b59eca212bb4d74a8f4a466b
Montywilliams <3650@holbertonschool.com>
2022-12-11 20:03:06 -0600

    add Categories and PostWidget to main container: tailwind styling used to for mobile display

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,17 +16,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => (
-          <PostCard  post={post} key={post.title} />
-        ))}
-      </div>
-          <div className="lg:col-span col-span-1">
-            <div>
-              <div className="lg:sticky relative top-8" >
-
-              </div>
+          <div className="lg:col-span-8 col-span-1">
+            {posts.map((post) => (<PostCard  post={post} key={post.title} />))}
+          </div>
+            <div className="lg:col-span-4 col-span-1">
+              <div className="lg:sticky relatve top-8" >
+                <PostWidget />
+                <Categories />
             </div>
           </div>
+      </div>
     </div>
   )
 }
