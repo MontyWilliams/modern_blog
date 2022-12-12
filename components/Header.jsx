fde781eccbcea9769d2a9dcb0ac6628e81c17c59
Montywilliams <3650@holbertonschool.com>
2022-12-12 09:36:24 -0600

    add categories and map through list

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useContext } from 'react'
+import Link from 'next/link';
 
 function Header() {
+
+  const categories = [{name: 'React', slug: 'React'}, {name: 'web development', slug: 'web development' }]
   return (
-    <div>Header</div>
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block border-blue-400 py-8">
+        <div className="md:float-left block">
+          <Link rel="stylesheet" href="/" >
+            <span className="cursor-pointer font-bold text-4xl text-white">
+              GraphCMS
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {catetgories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+
+        </div>
+      </div>
+    </div>
   )
 }
 
