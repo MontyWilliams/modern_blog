f96298705c7d4ec643008b7a0b7015f734efe5a8
Montywilliams <3650@holbertonschool.com>
2022-12-15 15:25:21 -0600

    create query and await result

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -2,7 +2,8 @@ import { GraphQLClient, gql } from 'graphql';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export default function comments(req, res) {
+export default async function comments(req, res) {
+
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
       authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
@@ -10,7 +11,10 @@ export default function comments(req, res) {
   })
 
   const query = gql`
-    mutation CreateBomment($name: String!, $email: String!, $comment: String!, $slug: String!)
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!){
       createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug}}}) { id }
-    `
+    }
+  `
+  const result = await graphQLClient.request(query, req.body)
+  return res.status(200).send(result)
 }
