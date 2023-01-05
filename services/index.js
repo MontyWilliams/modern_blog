bc1ed4c48a4a77abe333750f3638bbb53f6e10d3
Montywilliams <3650@holbertonschool.com>
2023-01-05 11:14:59 -0600

    add getFeatured post query

--- services/index.js
+++ services/index.js
@@ -150,3 +150,28 @@ export const getComments = async (slug) => {
   const result = await request(graphqlAPI, query, {slug});
   return result.comments;
 };
+
+export const getFeatured = async () => {
+  const query = gql`
+    query getFeaturedPosts {
+      posts(where: {featuredPost: true}) {
+        author {
+          name
+          photo {
+            url
+          }
+        }
+        featuredImage {
+          url
+        }
+        title
+        slug
+        createdAt
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
