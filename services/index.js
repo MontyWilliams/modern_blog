8966249ad1634117268f4a9a8e0088def73ea4ad
Montywilliams <3650@holbertonschool.com>
2023-01-04 20:46:45 -0600

    correct syntax error

--- services/index.js
+++ services/index.js
@@ -140,7 +140,7 @@ export const submitComment = async (obj) => {   //next js alpi is queried for co
 export const getComments = async (slug) => {
   const query = gql`
     query GetComments($slug: String!) {
-      Comments(where: {post: {slug: $slug}}) {
+      comments(where: {post: {slug: $slug}}) {
         name
         createdAt
         comment
