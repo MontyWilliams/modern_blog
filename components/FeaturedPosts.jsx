c391dc0b531be125d50589a06ec2b83cabcc7e97
Montywilliams <3650@holbertonschool.com>
2023-01-05 12:05:48 -0600

    correct import typo

--- components/FeaturedPosts.jsx
+++ components/FeaturedPosts.jsx
@@ -1,7 +1,28 @@
 import React, {useState, useEffect } from 'react'
 import Carousel from 'react-multi-carousel'
-import 'react-multi-carousel/lib/styles.css';
+import 'react-multi-carousel/lib/styles.css'
 import { getFeatured } from '../services'
+import { FeaturedPostsCard } from './' 
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 function FeaturedPosts() {
     const [featured, setFeatured] = useState([])
@@ -12,9 +33,13 @@ function FeaturedPosts() {
     }, []);
     console.log(featured)
   return (
-    <>
-        
-    </>
+    <div className="mb-8">
+        <Carousel infinite responsive={responsive}>
+            {featured.map((post, index) => (
+                <FeaturedPostsCard key={index} post={post} />
+            ))}
+        </Carousel>
+    </div>
   )
 }
 
