7f28c5738e8d29b9c3201ffe77a5de2bca06a0ed
Montywilliams <3650@holbertonschool.com>
2023-01-05 11:56:11 -0600

    create featured post card

---
+++ components/FeaturedPostsCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import moment from 'moment'
+import Image from 'next/image'
+import Link from 'next/link'
+
+function FeaturedPostsCard({ post }) {
+  return (
+    <div>FeaturedPostsCard</div>
+  )
+}
+
+export default FeaturedPostsCard
